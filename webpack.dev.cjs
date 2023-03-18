ba7cf4a1bbb53c0ceaea35b9a2ef006e0b9f76e8
pablomartan <pablomartan@kolabnow.com>
2023-03-18 10:38:19 +0100

    feat: add webpack configuration for dev and prod builds

---
+++ webpack.dev.cjs
@@ -0,0 +1,32 @@
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+const port = process.env.port || 3000;
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.[hash].js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/html/index.html',
+        })
+    ],
+    devServer: {
+        host: 'localhost',
+        port: port,
+        historyApiFallback: true,
+        open: true
+    }
+};
