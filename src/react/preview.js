2cf2656d09c4dbf6a64e10453385c30cde824bbe
pablomartan <pablomartan@kolabnow.com>
2023-03-18 11:16:03 +0100

    feat: add basic Preview react component in src/react/

---
+++ src/react/preview.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export class Preview extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return(
+      <div id="preview" />
+    )
+  }
+}
