0814c77b34f94badbe30d25e1a6abde55751d380
pablomartan <pablomartan@kolabnow.com>
2023-03-18 11:22:31 +0100

    feat: add basic redux skeleton

---
+++ src/redux/main.js
@@ -0,0 +1,24 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+const defaultState = {
+  'input': ''
+};
+
+// TODO: actions for input, parse, render
+
+// TODO: async function to parse md text in input
+
+// TODO: create reducer for actions
+
+const middleWarePlaceHolder = applyMiddleware(thunk);
+
+const reducer = (state = defaultState, action) => {
+  return state;
+};
+
+export const store = createStore(reducer);
+
+// TODO: export mapStateToProps
+
+// TODO: export mapDispatchToProps
