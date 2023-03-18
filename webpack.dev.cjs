45ab2b54c528799b179c588f9610d7aeaf385244
pablomartan <pablomartan@kolabnow.com>
2023-03-18 11:13:48 +0100

    fix: fix typo in webpack dev config file

--- webpack.dev.cjs
+++ webpack.dev.cjs
@@ -19,7 +19,7 @@ module.exports = {
         ]
     },
     plugins: [
-        new HtmlWebpackPlugin({
+        new HtmlWebPackPlugin({
             template: 'src/html/index.html',
         })
     ],
