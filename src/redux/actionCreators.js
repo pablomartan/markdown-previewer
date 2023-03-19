812b209f1ca1c6e08f18a43b26377add0f5e2074
pablomartan <pablomartan@kolabnow.com>
2023-03-19 10:58:19 +0100

    feat: add action creators file with action constant declarations

---
+++ src/redux/actionCreators.js
@@ -0,0 +1,11 @@
+/*
+ * Create actions for
+ *  - controlled text area
+ *  - parse input (submit)
+ *  - render parsed input
+*/
+
+// Declare actions
+const USER_INPUT = 'USER_INPUT';
+const PARSE = 'PARSE';
+const RENDER = 'RENDER';
