f3256f04efb54c48ef3e5007b8c7ec0efe2e2543
pablomartan <pablomartan@kolabnow.com>
2023-03-19 11:17:37 +0100

    feat: add action creator for user input to redux/

--- src/redux/actionCreators.js
+++ src/redux/actionCreators.js
@@ -9,3 +9,17 @@
 const USER_INPUT = 'USER_INPUT';
 const PARSE = 'PARSE';
 const RENDER = 'RENDER';
+
+/**
+ * @description: creates an action for user input and returns the text the user
+ * has input
+ * @param {Event} e: the CHANGE event
+ * @returns an action object with type USER_INPUT and the text of the text area
+ */
+export const inputChange = e => {
+  const value = e.target.value;
+  return {
+    'type': USER_INPUT,
+    'input': value
+  }
+};
