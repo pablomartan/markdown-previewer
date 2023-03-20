c60c3e491e5915afc1868925d68028fbc2d6cd6c
pablomartan <pablomartan@kolabnow.com>
2023-03-20 11:44:38 +0100

    'feat: several changes to redux/main.js'
    This commit places several changes. For starters, it adds a 'parsed'
    property to state. It also removes unused redux features like
    combineReducers and several comments

--- src/redux/main.js
+++ src/redux/main.js
@@ -1,41 +1,32 @@
-import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { inputChange } from './actionCreators.js';
 import * as Types from './actionTypes.js';
-import * as Reducers from './reducers.js';
 
 const defaultState = {
-  'input': ''
+  'input': '',
+  'parsed': ''
 };
-
-// TODO: actions for input, parse, render
-
-// TODO: async function to parse md text in input
-
-// TODO: create reducer for actions
 
 const middleWarePlaceHolder = applyMiddleware(thunk);
 
-const rootReducer = combineReducers(
-  {
-    inputChange: Reducers.inputReducer,
-    inputParse: Reducers.parseReducer,
-    inputRender: Reducers.renderReducer
-  }
-);
+const reducer = (state = defaultState, action) => {
+  return action.type === Types.USER_INPUT ? { 'input': action.input, 'parsed': action.parsed } : state;
+};
 
-export const store = createStore(rootReducer, middleWarePlaceHolder);
+export const store = createStore(reducer, middleWarePlaceHolder);
 
 export const mapDispatchToProps = dispatch => {
   return {
-    inputHandler: () => {
-      dispatch(inputChange());
+    inputHandler: e => {
+      dispatch(inputChange(e));
     }
   }
 };
 
 export const mapStateToProps = state => {
   return {
-    input: state.input
+    input: state.input,
+    parsed: state.parsed
   }
 };
