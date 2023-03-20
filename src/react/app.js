5fe559574966642283b354574783958ea69f3dd1
pablomartan <pablomartan@kolabnow.com>
2023-03-20 12:46:59 +0100

    feat: set app width to 75%

--- src/react/app.js
+++ src/react/app.js
@@ -9,7 +9,7 @@ export class App extends React.Component {
 
   render() {
     return(
-      <div id="app" className="row justify-content-center p-3">
+      <div id="app" className="row justify-content-center p-3 w-75">
         <Editor id="editor" input={this.props.input} handler={this.props.inputHandler}/>
         <Preview id="preview" text={this.props.parsed} />
       </div>
