7f3c6b197e9629cdfc3d922425ba150441fc6bac
pablomartan <pablomartan@kolabnow.com>
2023-03-20 11:41:43 +0100

    feat: import mapStateToProps into index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider, connect } from 'react-redux';
-import { store, mapDispatchToProps } from './redux/main.js';
+import { store, mapDispatchToProps, mapStateToProps } from './redux/main.js';
 import { App } from './react/app.js';
 
-const Present = connect(null, mapDispatchToProps)(App);
+const Present = connect(mapStateToProps, mapDispatchToProps)(App);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
