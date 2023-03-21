be228fb2ec2aedd021b91d3ac50659ed077a5a42
pablomartan <pablomartan@kolabnow.com>
2023-03-21 12:17:13 +0100

    feat: add custom stylesheet to webpack entry point

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { Provider, connect } from 'react-redux';
 import { store, mapDispatchToProps, mapStateToProps } from './redux/main.js';
 import { App } from './react/app.js';
 
+import custom from './style/custom.scss';
+
 marked.setOptions({ breaks: true });
 
 const Present = connect(mapStateToProps, mapDispatchToProps)(App);
